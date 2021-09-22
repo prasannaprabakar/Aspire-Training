385a72bb8ab3f8008beb582a43785ce0271de18f
prasanna <52930499+prasannaprabakar@users.noreply.github.com>
2021-09-22 11:30:12 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Properties prop = System.getProperties();
+		prop.list(System.out);
+	}
+
+}
